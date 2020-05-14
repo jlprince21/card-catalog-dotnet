0603b31f7922607a3dc64029948f46a3ca7bb7c3
Luke Prince <luke@theprinces.org>
2020-05-13 21:38:15 -0400

    Add CLI  project back

---
+++ CardCatalog.Terminal/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CardCatalog.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace CardCatalog.Terminal
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<CardCatalogContext>();
+                optionsBuilder.UseSqlite("Data Source=cc_dev.db");
+
+                CardCatalogContext dbContext = new CardCatalogContext(optionsBuilder.Options);
+
+                var y = new FileProcessing(dbContext);
+
+                await y.ScanFiles("/some/path");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message + ex.InnerException);
+            }
+        }
+    }
+}
