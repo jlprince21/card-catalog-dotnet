57cf7dcd0134019fbecdbffc32a29060afec5418
Luke Prince <luke@theprinces.org>
2020-05-17 08:38:49 -0400

    Merge item inventory tables into project

---
+++ CardCatalog.Core/Migrations/20200516111149_MergeWhereIsIt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CardCatalog.Core.Migrations
+{
+    public partial class MergeWhereIsIt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    Container = table.Column<Guid>(nullable: false),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Containers_Container",
+                        column: x => x.Container,
+                        principalTable: "Containers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_Container",
+                table: "Items",
+                column: "Container");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Containers");
+        }
+    }
+}
--- CardCatalog.Core/Model.cs
+++ CardCatalog.Core/Model.cs
@@ -2,6 +2,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
 using System.Linq;
 
 namespace CardCatalog.Core
@@ -11,6 +12,8 @@ namespace CardCatalog.Core
         public DbSet<Listing> Listings { get; set; }
         public DbSet<ListingTag> ListingTags { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Container> Containers { get; set; }
 
         // NOTE 2020-05-02 This constructor and OnConfiguring were used when the
         // CLI project was main way of interacting with the Core project. If troubles
@@ -27,6 +30,12 @@ namespace CardCatalog.Core
         // protected override void OnConfiguring(DbContextOptionsBuilder options)
         //     => options.UseSqlite("Data Source=card_catalog.db");
 
+        public CardCatalogContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=" + ConfigurationManager.AppSettings["DatabasePath"]);
+        }
+
         public CardCatalogContext(DbContextOptions<CardCatalogContext> options) : base(options)
         {
         }
@@ -36,6 +45,8 @@ namespace CardCatalog.Core
             modelBuilder.Entity<Listing>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
             modelBuilder.Entity<Tag>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
             modelBuilder.Entity<ListingTag>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<Item>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<Container>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
 
             // this is needed to set a unique constraint on TagTitle column in Tags table
             modelBuilder.Entity<Tag>()
@@ -93,5 +104,29 @@ namespace CardCatalog.Core
         [ForeignKey("Tag")]
         [Required]
         public Tag TagRefId { get; set; }
+    }
+
+    public class Item
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("Container")]
+        [Required]
+        public Container ContainerRefId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+
+    public class Container
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
     }
 }
